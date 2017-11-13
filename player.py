c88f1588ed81a18c1bc2525b2967571a498e83ea
Robert <Robert@DESKTOP-PPGOJSQ>
2017-11-13 16:43:04 +0100

    üd9rgühdg

---
+++ player.py
@@ -0,0 +1,16 @@
+class Player(object):
+
+    def __init__(self, name, id, score=0, dice_roll_counter=0):
+        self.name = name
+        self.score = score
+        self.dice_roll_counter = dice_roll_counter
+        self.id = id
+
+    def pri(self):
+        print("New Obj", self.name)
+
+    def setscore(self, score):
+        self.score += score
+    
+    def getscore(self):
+        return (Self.score)
