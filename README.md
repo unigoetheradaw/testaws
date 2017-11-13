a5538c78bc78049982790dd88ee964c0194553f7
Robert <Robert@DESKTOP-PPGOJSQ>
2017-11-13 13:32:01 +0100

    Anmerkung EPR

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ getch() <conio.h> : Diese Funktion ist defakto Standard und funktioniert ähnlic
 ord(c)
 Given a string of length one, return an integer representing the Unicode code point of the character when the argument is a unicode object, or the value of the byte when the argument is an 8-bit string. For example, ord('a') returns the integer 97, ord(u'\u2020') returns 8224. This is the inverse of chr() for 8-bit strings and of unichr() for unicode objects. If a unicode argument is given and Python was built with UCS2 Unicode, then the character’s code point must be in the range [0..65535] inclusive; otherwise the string length is two, and a TypeError will be raised.
 
+To do:
+Spieler Objekte von Klasse Player?
+
 key = ord(getch())
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """Das ist ein DocString"""
 import os
 import time
-from random import *
+import random
 
 
 def clearscreen():
@@ -13,7 +13,7 @@ def welcome():
     print("==============16 ist tot==============")
     print("|.............:Willkommen:...........|")
     print("|....................................|")
-    print("|.Press (1)..Menue...................|")
+    print("|.Press (1)..Configure your Game.....|")
     print("|.Press (2)..Auto-Game...............|")
     print("|.Press (3)..Help....................|")
     print("|.Press (4)..Exit....................|")
@@ -21,22 +21,50 @@ def welcome():
 
 
 def role_dice(number=1, faces=6, seed=None):
-    print(randint(1,6))
+    print("LOLOLOL Seed:", seed)
+    random.seed(seed)
+    c = random.randint(1, faces)
+    random.seed(seed)
+    b = random.randint(1, faces)
+    random.seed(seed)
+    a = random.randint(1, faces)
+    return (a, b, c)
+
+
+def configure_game():
+    print("Category choose: Configure your Game")
+    print("Enter number of Players")
+    players_number = int(input())  # generate player objects
+    print("Enter number of Dices")
+    dices_number = int(input())  # catch errors
+    print("Enter number of Faces of Dices")
+    faces_number = int(input())  # catch_errors
+    print("Do you want to set a seed (y/n)")
+    # perhaps override seed in role_dice
+    seed = input()
+    role_dice(dices_number, faces_number, seed)
+
+    
+def auto_game():
+    return(role_dice())
+
+    
+def help():
+    potato
+
 
 def somefunc():
     welcome()
+    seed = 5
     while(True):
         a = input()
         print(a)
-        if(a == "4"):
+        if (a == "4"):
             break
-        if (a == "3"):
-            print(role_dice)
-            time.sleep(3)
-            print(role_dice)
-            time.sleep(3)
-            print(role_dice)
-            time.sleep(3)
+        if (a == "1"):
+             configure_game()
+        if (a == "2"):
+            print(auto_game())
 
 
 somefunc()
